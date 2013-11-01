6cbf6b49baf40da902e3494cf8cdfe7831b27488
Gingber <kmust.jiangbo@gmail.com>
2013-11-01 15:00:24 +0800

    RetweetNetworkGraph
    Graph Node is changed

--- src/Analyzer/centrality/examples/RetweetNetworkGraph.java
+++ src/Analyzer/centrality/examples/RetweetNetworkGraph.java
@@ -40,7 +40,7 @@ public class RetweetNetworkGraph {
 			tweetFile = new File(args[0]);
 		}
 		else{
-			tweetFile = new File("file/BarackObama.json");
+			tweetFile = new File("file/DreamD.json");
 		}
 		
 		DirectedGraph<UserNode, RetweetEdge> retweetGraph = TweetFileToGraph.getRetweetNetwork(tweetFile);
@@ -62,75 +62,43 @@ public class RetweetNetworkGraph {
 			Vector<RetweetObject> clusterRtObjs = new Vector<RetweetObject>();
 			Vector<Integer> SameContent = new Vector<Integer>();
 			
-			clusterRtObj = new RetweetObject(rtobjs.get(i).getFromUserName(), rtobjs.get(i).getOriginalDate(), rtobjs.get(i).getToUserName(), rtobjs.get(i).getRetweetDate());
+			clusterRtObj = new RetweetObject(rtobjs.get(i).getFromMsgId(), rtobjs.get(i).getFromUserName(), rtobjs.get(i).getOriginalDate(), rtobjs.get(i).getToMsgId(), rtobjs.get(i).getToUserName(), rtobjs.get(i).getRetweetDate());
 			clusterRtObjs.add(clusterRtObj);
 			
-			int RtNum = 0;
-			for(int j = i+1; j < rtobjs.size(); j++) 
-			{
-				if(rtobjs.get(i).getFromMsgId().equals(rtobjs.get(j).getFromMsgId()))	// 1->2, 1->3
-				{
-					clusterRtObj = new RetweetObject(rtobjs.get(j).getFromUserName(), rtobjs.get(j).getOriginalDate(), rtobjs.get(j).getToUserName(), rtobjs.get(j).getRetweetDate());
-					clusterRtObjs.add(clusterRtObj);
-					
-					if(!SameContent.contains(i)) {
-						SameContent.add(i);
-					}
-					
-					if(!SameContent.contains(j)) {
-						SameContent.add(j);
-					}
-					
-					RtNum++;
-				}
-				
-				if(rtobjs.get(i).getToMsgId().equals(rtobjs.get(j).getFromMsgId()))	// 1->2, 2->3
-				{
-					clusterRtObj = new RetweetObject(rtobjs.get(j).getFromUserName(), rtobjs.get(j).getOriginalDate(), rtobjs.get(j).getToUserName(), rtobjs.get(j).getRetweetDate());
-					clusterRtObjs.add(clusterRtObj);
-					
-					if(!SameContent.contains(i)) {
-						SameContent.add(i);
-					}
-					
-					if(!SameContent.contains(j)) {
-						SameContent.add(j);
-					}
-					
-					RtNum++;
-				}
-				
-				if(rtobjs.get(i).getToMsgId().equals(rtobjs.get(j).getFromMsgId()))	// 1->2, 3->1
-				{
-					clusterRtObj = new RetweetObject(rtobjs.get(j).getFromUserName(), rtobjs.get(j).getOriginalDate(), rtobjs.get(j).getToUserName(), rtobjs.get(j).getRetweetDate());
-					clusterRtObjs.add(clusterRtObj);
-					
-					if(!SameContent.contains(i)) {
-						SameContent.add(i);
-					}
-					
-					if(!SameContent.contains(j)) {
-						SameContent.add(j);
-					}
-					
-					RtNum++;
-				}	
-			}
+			Vector<String> Node = new Vector<String>();
 			
-			if(RtNum > 0)
-			{			
-				RtNum++;
-				
-				clusterRtObj = new RetweetObject(rtobjs.get(i).getFromUserName(), rtobjs.get(i).getOriginalDate(), rtobjs.get(i).getToUserName(), rtobjs.get(i).getRetweetDate());
-				clusterRtObjs.add(clusterRtObj);
+			Node.add(rtobjs.get(i).getFromMsgId());
+			Node.add(rtobjs.get(i).getToMsgId());
+			
+			int RtNum = 0;
+			for(int j = i+1; j < rtobjs.size(); j++) {
+				if (Node.contains(rtobjs.get(j).getFromMsgId())) {
+					
+					clusterRtObj = new RetweetObject(rtobjs.get(j).getFromUserName(), rtobjs.get(j).getOriginalDate(), rtobjs.get(j).getToUserName(), rtobjs.get(j).getRetweetDate());
+					clusterRtObjs.add(clusterRtObj);
+					
+					if(!Node.contains(rtobjs.get(j).getToMsgId())) {
+						Node.add(rtobjs.get(j).getToMsgId());					
+					}
+					
+					if(!SameContent.contains(i)) {
+						SameContent.add(i);
+					}
+					
+					if(!SameContent.contains(j)) {
+						SameContent.add(j);
+					}
+					RtNum++;
+					
+				}
 			}
-
+				
 			// 转发次数达到一定阈值，则写入文件
 			if(clusterRtObjs.size() != 0 && RtNum > 1000) 
 			{
-				/*Comparator datecomp = new DateComparatorImplementation();
+				Comparator datecomp = new RtComparatorImplementation();
 				Collections.sort(clusterRtObjs, datecomp);	//按时间进行升序排列
-*/				StringBuilder RtCluster = new StringBuilder();
+				StringBuilder RtCluster = new StringBuilder();
 				RtCluster.append(rtobjs.get(i).getFromMsgId());
 				RtCluster.append("\n");
 				RtCluster.append(rtobjs.get(i).getFromUserName());
@@ -148,30 +116,20 @@ public class RetweetNetworkGraph {
 					RtCluster.append(clusterRtObjs.get(k).getRetweetDate());
 					RtCluster.append("\n");
 				}
-				TxtWriter.saveToFile(RtCluster.toString(), new File("file/RetweetGraph/DreamD_" + i+ ".csv"), "UTF-8");
+				TxtWriter.saveToFile(RtCluster.toString(), new File("file/RetweetGraph/DreamD" + i+ ".csv"), "UTF-8");
 			}
 			
 			if (SameContent != null && SameContent.size() > 0) // 存在重复项
 			{ 
-				for (int j = SameContent.size() - 1; j >= 0; j--) 
-				{
-					int index = Integer.parseInt(SameContent.get(j)
+				for (int k = SameContent.size() - 1; k >= 0; k--) {
+					int index = Integer.parseInt(SameContent.get(k)
 							.toString());
 					rtobjs.remove(index);
 				}
-			} 
+			}
 		}
 	}
 }
-
-/*class ComparatorImplementation implements Comparator<RetweetObject> {
-	public static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	public int compare(RetweetObject s1,RetweetObject s2) {
-		Long id_1 = Long.parseLong(s1.getFromMsgId());
-		Long id_2 = Long.parseLong(s2.getFromMsgId());
-		return id_1.compareTo(id_2);
-	}
-}*/
 
 class ComparatorImplementation implements Comparator<RetweetObject> {
 	public static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -186,6 +144,32 @@ class ComparatorImplementation implements Comparator<RetweetObject> {
 		Date end = null;
 		try {
 			end = sdf.parse(s2.getOriginalDate());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(begin.after(end)){  
+            return 1;  
+        }  
+        else{  
+            return -1;  
+        }    
+	}
+}
+
+class RtComparatorImplementation implements Comparator<RetweetObject> {
+	public static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	public int compare(RetweetObject s1,RetweetObject s2) {
+		Date begin = null;
+		try {
+			begin = sdf.parse(s1.getRetweetDate());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Date end = null;
+		try {
+			end = sdf.parse(s2.getRetweetDate());
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
